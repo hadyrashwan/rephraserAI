1333fae8c402d2a6155bd41bf62b1189a2d4127a
Hady Rashwan <h2rashwan@gmail.com>
2024-11-08 09:22:26 +0000

    feat: Add storage permission and host permissions to manifest

--- background.js
+++ background.js
@@ -10,6 +10,7 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === "rephrase") {
     const selectedText = info.selectionText;
 
+
     // Load API key and model from Chrome storage
     chrome.storage.sync.get(['apiKey', 'model'], (data) => {
       const apiKey = data.apiKey || 'AIzaSyCR4Y7xry-5rz_m4IY51J2urBsAGsVw35o';
@@ -29,6 +30,7 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
       .then(response => response.json())
       .then(data => {
         const rephrasedText = data.candidates[0].content.parts[0].text;
+        console.log(rephrasedText)
         chrome.runtime.sendMessage({ action: 'showPopup', text: rephrasedText });
       })
       .catch(error => console.error('Error:', error));
